796eb0b3d36305c532178c7e46a84b315b16c78e
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-01-20 15:57:00 +0100

    no instances

--- tests/index.js
+++ tests/index.js
@@ -2,19 +2,13 @@ import should from 'should/as-function'
 
 import { DNSNode, ExternalNode } from '../src'
 
-const DNSHOST = process.env.DNSHOST
-
 describe('DNS-MessageBus', () => {
   it('should be fun', () => {})
   it('should export a DNSNode factory function', () => {
     should(DNSNode).be.a.function
-    let dnsNode = DNSNode(DNSHOST)
-    should(dnsNode).be.an.instanceof(DNSNode)
   })
   it('should export an ExternalNode factory function', () => {
     should(ExternalNode).be.a.function
-    let externalNode = ExternalNode(DNSHOST)
-    should(externalNode).be.an.instanceof(ExternalNode)
   })
 
   require('./DNSNode-tests')
