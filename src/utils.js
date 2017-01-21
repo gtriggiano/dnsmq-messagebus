601cbed79338fd3831350e42d0890b74a5ddd6a0
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-01-21 17:53:17 +0100

    implemented utils.getSocketPort

--- src/utils.js
+++ src/utils.js
@@ -2,11 +2,11 @@ import os from 'os'
 import { ZMQ_LAST_ENDPOINT } from 'zeromq'
 import { curry } from 'lodash'
 
-const prefixString = curry(function prefixString (prefix, str) {
+export const prefixString = curry(function prefixString (prefix, str) {
   return `${prefix}${str}`
 })
 
-function nodeIdToName (id) { return id.substring(3, 11) }
+export function nodeIdToName (id) { return id.substring(3, 11) }
 
 function boxExternalIps () {
   let ifaces = os.networkInterfaces()
@@ -15,23 +15,21 @@ function boxExternalIps () {
   }, []).map(({address}) => address)
 }
 
-function getSocketEndpoint (socket) {
+export function getSocketEndpoint (socket) {
   let address = socket.getsockopt(ZMQ_LAST_ENDPOINT)
   let ip = boxExternalIps()[0]
   return ip ? address.replace(/0\.0\.0\.0/, ip) : undefined
 }
 
-function zeropad (num, len) {
+export function getSocketPort (socket) {
+  let address = socket.getsockopt(ZMQ_LAST_ENDPOINT)
+  return address.replace(/tcp:\/\/0\.0\.0\.0:/, '')
+}
+
+export function zeropad (num, len) {
   let str = String(num)
   while (str.length < len) {
     str = `0${str}`
   }
   return str
-}
-
-export {
-  nodeIdToName,
-  prefixString,
-  getSocketEndpoint,
-  zeropad
 }
