fc39e5a3c9d1c08ddef527f8357b312e57f31480
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-01-21 17:56:12 +0100

    MasterMessagesBroker knows about port but not IP

--- src/MasterMessagesBroker.js
+++ src/MasterMessagesBroker.js
@@ -1,6 +1,6 @@
 import zmq from 'zeromq'
 
-import { getSocketEndpoint } from './utils'
+import { nodeIdToName, getSocketPort } from './utils'
 
 const HEARTBEAT_INTERVAL = 500
 
@@ -13,19 +13,20 @@ function getSockets () {
   return {sub: _sub, pub: _pub}
 }
 
-function MasterMessagesBroker (id) {
+function MasterMessagesBroker ({id}) {
+  let _ip
   let _sub
   let _pub
   let _bound
-  let _subAddress
-  let _pubAddress
+  let _subPort
+  let _pubPort
   let _hearbeatInterval
 
   function _sendHeartbeat () {
     if (!_bound) return
     _pub.send(['heartbeats', JSON.stringify({
-      id,
-      endpoints: {sub: _subAddress, pub: _pubAddress}
+      name: nodeIdToName(id),
+      endpoints: broker.endpoints
     })])
   }
 
@@ -33,20 +34,20 @@ function MasterMessagesBroker (id) {
     bind () {
       if (_bound) return
       let {sub, pub} = getSockets()
-      sub.bindSync('tcp://0.0.0.0:*')
-      pub.bindSync('tcp://0.0.0.0:*')
+      sub.bindSync(`tcp://0.0.0.0:*`)
+      pub.bindSync(`tcp://0.0.0.0:*`)
       _sub = sub
       _pub = pub
-      _subAddress = getSocketEndpoint(_sub)
-      _pubAddress = getSocketEndpoint(_pub)
+      _subPort = getSocketPort(_sub)
+      _pubPort = getSocketPort(_pub)
       _bound = true
     },
     unbind () {
       if (!_bound) return
       _sub.close()
       _pub.close()
-      _subAddress = null
-      _pubAddress = null
+      _subPort = null
+      _pubPort = null
       _bound = false
     },
     startHeartbeats () {
@@ -60,12 +61,25 @@ function MasterMessagesBroker (id) {
     }
   }
 
-  Object.defineProperty(broker, 'endpoints', {
-    get: () => ({sub: _subAddress, pub: _pubAddress}),
-    set: () => {}
+  return Object.defineProperties(broker, {
+    setIP: {
+      value: (ip) => {
+        _ip = _ip || ip
+      }
+    },
+    endpoints: {
+      get: () => ({
+        sub: _ip ? `tcp://${_ip}:${_subPort}` : undefined,
+        pub: _ip ? `tcp://${_ip}:${_pubPort}` : undefined
+      })
+    },
+    ports: {
+      get: () => ({
+        sub: _subPort,
+        pub: _pubPort
+      })
+    }
   })
-
-  return broker
 }
 
 export default MasterMessagesBroker
