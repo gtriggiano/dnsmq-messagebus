59ea5e0232c790f4d251839e47b68c2d7c697f5b
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-01-31 12:17:53 +0100

    constants file

---
+++ src/Constants.js
@@ -0,0 +1,6 @@
+export const DNS_NODE = 'DNSNode'
+export const EXTERNAL_NODE = 'ExternalNode'
+
+export const HEARTBEAT_INTERVAL = 350
+export const HEARTBEAT_INTERVAL_CHECK = 200
+export const HEARTBEAT_TIMEOUT = 1000
