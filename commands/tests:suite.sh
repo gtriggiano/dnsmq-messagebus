82f62d0b00838324b6384a2c4ac3b6cdd47272a0
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-02-01 11:57:28 +0100

    tests suite

---
+++ commands/tests:suite.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+set -e
+
+source "${BASH_SOURCE%/*}/common.sh"
+
+echo "dnsmq-messagebus package tests suite"
+echo
+echo "Execution order:"
+echo " - UNIT TESTS"
+echo " - DNS NODE TESTS"
+echo
+echo -n "Transpiling package and tests... "
+source "${BASH_SOURCE%/*}/transpile.sh" &> /dev/null
+cleanContainers &>/dev/null
+echo "Done."
+echo
+echo
+echo "UNIT TESTS"
+echo
+runAsService development better-npm-run test:unit
+echo
+separator
+echo
+echo -n "Starting bus service..."
+startService bus &>/dev/null
+sleep 2
+echo "Done."
+echo
+separator
+echo
+echo "DNS NODE TESTS"
+runAsService bus better-npm-run test:dnsnode
+echo
+separator
+echo
+echo "EXTERNAL NODE TESTS"
+echo
+runAsService bus better-npm-run test:externalnode
+echo
+separator
+echo
+echo -n "Cleaning bus service..."
+cleanService bus
+echo "Done."
+echo
