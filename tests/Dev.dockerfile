f3030b77a513b08c392b6c5577eb60607b99721c
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-02-01 12:56:27 +0100

    clean

--- tests/Dev.dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:argon-slim
-
-WORKDIR /app
-
-ADD package.json /app/package.json
-RUN npm install
--- tests/startClusterNode.js
+++
@@ -1,7 +0,0 @@
-import { DNSNode } from '../src'
-
-let messagebus = DNSNode('cluster', {debug: true})
-messagebus.connect()
-messagebus.on('connect', () => {
-  messagebus.debug('CONNECTED')
-})
