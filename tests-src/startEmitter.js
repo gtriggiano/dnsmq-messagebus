5d8c6a0fd1a39351ac53fe8da018262ae77e9d97
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-02-01 16:49:42 +0100

    test emitter program

---
+++ tests-src/startEmitter.js
@@ -0,0 +1,35 @@
+const codePath = `../${process.env.CODE_PATH}`
+const DNSHOST = 'bus'
+
+var Node = require(codePath).Node
+
+var node = Node(DNSHOST, {external: true})
+
+var _canPublish = false
+node.on('can:publish', () => { _canPublish = true })
+node.on('cannot:publish', () => { _canPublish = false })
+
+var messageNo = 0
+var emissionInterval = setInterval(() => {
+  switch (_canPublish) {
+    case true:
+      messageNo++
+      node.publish('testchannel', node.name, messageNo)
+      break
+    default:
+      console.log()
+      console.log(`Cannot publish, will delay emission of message ${messageNo + 1}`)
+      console.log()
+  }
+}, 300)
+
+node.activate()
+
+process.on('SIGTERM', shutDown)
+process.on('SIGINT', shutDown)
+
+function shutDown () {
+  clearInterval(emissionInterval)
+  node.on('deactivated', () => process.exit(0))
+  node.deactivate()
+}
