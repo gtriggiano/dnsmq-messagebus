ab935cae537f8a7e61a6ed6fba247661281c9374
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-02-02 15:32:10 +0100

    tests reorg

--- tests-src/behaviour.js
+++ tests-src/behaviour.js
@@ -76,114 +76,6 @@ describe(`Connectivity of ${EXTERNAL ? 'an ExternalNode' : 'a DNSNode'}`, () => 
     })
   }
 })
-describe(`Properties of ${EXTERNAL ? 'an ExternalNode' : 'a DNSNode'} connectivity state`, () => {
-  it('node.canPublish is true/false depending on the state of the publishing connection to master', (done) => {
-    let node = Node()
-    should(node.canPublish).be.False()
-    node.on('ready', () => {
-      should(node.canPublish).be.True()
-      node.deactivate()
-    })
-    node.once('deactivated', () => {
-      should(node.canPublish).be.False()
-      done()
-    })
-    node.activate()
-  })
-  it('node.isReceiving is true/false depending on the state of the subscribing connection to master', (done) => {
-    let node = Node()
-    should(node.isReceiving).be.False()
-    node.on('ready', () => {
-      should(node.isReceiving).be.True()
-      node.deactivate()
-    })
-    node.once('deactivated', () => {
-      should(node.isReceiving).be.False()
-      done()
-    })
-    node.activate()
-  })
-  it('node.isReady is true/false depending on the states of both the subscribing and the publishing connections to master', (done) => {
-    let node = Node()
-    should(node.isReady).be.False()
-    node.on('ready', () => {
-      should(node.isReady).be.True()
-      node.deactivate()
-    })
-    node.once('deactivated', () => {
-      should(node.isReady).be.False()
-      done()
-    })
-    node.activate()
-  })
-  it('node.subscribedChannels provides a list of channels', () => {
-    let node = Node()
-    node.subscribe('a')
-    node.subscribe(['b', 'c'])
-    should(node.subscribedChannels).be.an.Array()
-    should(node.subscribedChannels.sort()).eql(['a', 'b', 'c'])
-  })
-  it('node.subscribedChannels is not settable nor mutable', () => {
-    let node = Node()
-    node.subscribe('a')
-
-    should(() => { node.subscribedChannels = [] }).throw()
-    node.subscribedChannels.push('b')
-    should(node.subscribedChannels.length).equal(1)
-  })
-  it('node.master provides an informational object about the master that the node is connected to', (done) => {
-    let node = Node()
-    should(node.master).be.Null()
-    node.on('ready', () => {
-      should(node.master.name).be.a.String()
-      should(node.master.endpoints.sub).be.a.String()
-      should(node.master.endpoints.pub).be.a.String()
-      node.deactivate()
-    })
-    node.on('not:ready', () => {
-      done()
-    })
-    node.activate()
-  })
-  it('node.master is not settable nor mutable', (done) => {
-    let node = Node()
-    should(() => { node.master = {} }).throw()
-    node.on('ready', () => {
-      node.master.prop = true
-      node.master.endpoints.prop = true
-      should(node.master.prop).be.Undefined()
-      should(node.master.endpoints.prop).be.Undefined()
-      node.deactivate()
-    })
-    node.on('deactivated', () => done())
-    node.activate()
-  })
-  it('node.isMaster is a boolean', () => {
-    let node = Node()
-    should(node.isMaster).be.a.Boolean()
-  })
-  if (!EXTERNAL) {
-    it('node.isMaster is true when the node has been elected master', function (done) {
-      this.timeout(10000)
-
-      let node = Node()
-      should(node.isMaster).be.False()
-      node.activate()
-      node.once('ready', () => {
-        restartBusNode1()
-        .then(() => {
-          should(node.isMaster).be.True()
-          node.deactivate()
-        })
-        .catch(done)
-      })
-      node.once('deactivated', () => {
-        should(node.isMaster).be.False()
-        done()
-      })
-    })
-  }
-})
 describe(`As an EventEmitter ${EXTERNAL ? 'an ExternalNode' : 'a DNSNode'}`, () => {
   it('emits a `ready` event when the node connects to the master', (done) => {
     let node = Node()
@@ -255,4 +147,115 @@ describe(`As an EventEmitter ${EXTERNAL ? 'an ExternalNode' : 'a DNSNode'}`, () 
     })
     node.activate()
   })
+})
+describe(`As a messagebus node ${EXTERNAL ? 'an ExternalNode' : 'a DNSNode'}`, () => {
+  it('can subscribe to channels')
+  it('can unsubscribe from channels')
+  it('can publish on the bus')
+  it('exposes a list of subscribed channels through node.subscribedChannels', () => {
+    let node = Node()
+    node.subscribe('a')
+    node.subscribe(['b', 'c'])
+    should(node.subscribedChannels).be.an.Array()
+    should(node.subscribedChannels.sort()).eql(['a', 'b', 'c'])
+  })
+  it('node.subscribedChannels is not settable nor mutable', () => {
+    let node = Node()
+    node.subscribe('a')
+
+    should(() => { node.subscribedChannels = [] }).throw()
+    node.subscribedChannels.push('b')
+    should(node.subscribedChannels.length).equal(1)
+  })
+  it('has prop node.canPublish: true/false depending on the state of the publishing connection to master', (done) => {
+    let node = Node()
+    should(node.canPublish).be.False()
+    node.on('ready', () => {
+      should(node.canPublish).be.True()
+      node.deactivate()
+    })
+    node.once('deactivated', () => {
+      should(node.canPublish).be.False()
+      done()
+    })
+    node.activate()
+  })
+  it('has prop node.isReceiving: true/false depending on the state of the subscribing connection to master', (done) => {
+    let node = Node()
+    should(node.isReceiving).be.False()
+    node.on('ready', () => {
+      should(node.isReceiving).be.True()
+      node.deactivate()
+    })
+    node.once('deactivated', () => {
+      should(node.isReceiving).be.False()
+      done()
+    })
+    node.activate()
+  })
+  it('has prop node.isReady: true/false depending on the states of both the subscribing and the publishing connections to master', (done) => {
+    let node = Node()
+    should(node.isReady).be.False()
+    node.on('ready', () => {
+      should(node.isReady).be.True()
+      node.deactivate()
+    })
+    node.once('deactivated', () => {
+      should(node.isReady).be.False()
+      done()
+    })
+    node.activate()
+  })
+  it('has prop node.master: null or an informational object about the master that the node is connected to', (done) => {
+    let node = Node()
+    should(node.master).be.Null()
+    node.on('ready', () => {
+      should(node.master.name).be.a.String()
+      should(node.master.endpoints.sub).be.a.String()
+      should(node.master.endpoints.pub).be.a.String()
+      node.deactivate()
+    })
+    node.on('not:ready', () => {
+      done()
+    })
+    node.activate()
+  })
+  it('prop node.master is not settable nor mutable', (done) => {
+    let node = Node()
+    should(() => { node.master = {} }).throw()
+    node.on('ready', () => {
+      node.master.prop = true
+      node.master.endpoints.prop = true
+      should(node.master.prop).be.Undefined()
+      should(node.master.endpoints.prop).be.Undefined()
+      node.deactivate()
+    })
+    node.on('deactivated', () => done())
+    node.activate()
+  })
+  it('has prop node.isMaster: is a boolean', () => {
+    let node = Node()
+    should(node.isMaster).be.a.Boolean()
+  })
+  if (!EXTERNAL) {
+    it('node.isMaster is true while the node has the master role', function (done) {
+      this.timeout(10000)
+
+      let node = Node()
+      should(node.isMaster).be.False()
+      node.activate()
+      node.once('ready', () => {
+        restartBusNode1()
+        .then(() => {
+          should(node.isMaster).be.True()
+          node.deactivate()
+        })
+        .catch(done)
+      })
+      node.once('deactivated', () => {
+        should(node.isMaster).be.False()
+        done()
+      })
+    })
+  }
 })
